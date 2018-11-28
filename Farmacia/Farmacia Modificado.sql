b5fc40f24b198c5bc632320a08a29a37bea146c1
PanchoMen <32570447+PanchoMen@users.noreply.github.com>
2018-11-28 10:17:40 +0000

    Añadidas Capturas

--- Farmacia/Farmacia Modificado.sql
+++ Farmacia/Farmacia Modificado.sql
@@ -8,9 +8,9 @@
 CREATE SCHEMA IF NOT EXISTS Farmacia;
 
 -- -----------------------------------------------------
--- Table Farmacia.Laborartorio
+-- Table Laborartorio
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS Farmacia.Laborartorio (
+CREATE TABLE IF NOT EXISTS Laborartorio (
   Direccion VARCHAR(45) NULL,
   Fax INT NULL,
   Telefono INT NULL,
@@ -22,9 +22,9 @@ CREATE TABLE IF NOT EXISTS Farmacia.Laborartorio (
 
 
 -- -----------------------------------------------------
--- Table Farmacia.Medicamento
+-- Table Medicamento
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS Farmacia.Medicamento (
+CREATE TABLE IF NOT EXISTS Medicamento (
   Precio INT NULL,
   Tipo VARCHAR(45) NULL,
   Stock INT NULL,
@@ -37,9 +37,9 @@ CREATE TABLE IF NOT EXISTS Farmacia.Medicamento (
 
 
 -- -----------------------------------------------------
--- Table Farmacia.Familia
+-- Table Familia
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS Farmacia.Familia (
+CREATE TABLE IF NOT EXISTS Familia (
   Descripción VARCHAR(100) NULL,
   Nomb_Familia VARCHAR(45) NOT NULL,
   PRIMARY KEY (Nomb_Familia));
@@ -47,9 +47,9 @@ CREATE TABLE IF NOT EXISTS Farmacia.Familia (
 
 
 -- -----------------------------------------------------
--- Table Farmacia.Compra
+-- Table Compra
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS Farmacia.Compra (
+CREATE TABLE IF NOT EXISTS Compra (
   Cod_Compra INT NOT NULL,
   Fecha TIMESTAMP(6) NULL,
   Importe DECIMAL NULL,
@@ -58,9 +58,9 @@ CREATE TABLE IF NOT EXISTS Farmacia.Compra (
 
 
 -- -----------------------------------------------------
--- Table Farmacia.C_Normal
+-- Table C_Normal
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS Farmacia.C_Normal (
+CREATE TABLE IF NOT EXISTS C_Normal (
   DNI VARCHAR(9) NOT NULL,
   Telefono INT NULL,
   Direccion VARCHAR(45) NULL,
@@ -73,9 +73,9 @@ CREATE TABLE IF NOT EXISTS Farmacia.C_Normal (
 
 
 -- -----------------------------------------------------
--- Table Farmacia.Con_Credito
+-- Table Con_Credito
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS Farmacia.Con_Credito (
+CREATE TABLE IF NOT EXISTS Con_Credito (
   DNI VARCHAR(9) NOT NULL,
   Telefono INT NULL,
   Direccion VARCHAR(45) NULL,
@@ -85,100 +85,100 @@ CREATE TABLE IF NOT EXISTS Farmacia.Con_Credito (
 
 
 -- -----------------------------------------------------
--- Table Farmacia.Lab_Med
+-- Table Lab_Med
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS Farmacia.Lab_Med (
+CREATE TABLE IF NOT EXISTS Lab_Med (
   Cod_Laboratorio VARCHAR(45) NOT NULL,
   Cod_Barras VARCHAR(45) NOT NULL,
   PRIMARY KEY (Cod_Laboratorio, Cod_Barras),
   CONSTRAINT Cod_Laboratorio
     FOREIGN KEY (Cod_Laboratorio)
-    REFERENCES Farmacia.Laborartorio (Cod_Laboratorio)
+    REFERENCES Laborartorio (Cod_Laboratorio)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT Cod_Barras
     FOREIGN KEY (Cod_Barras)
-    REFERENCES Farmacia.Medicamento (Cod_Barras)
+    REFERENCES Medicamento (Cod_Barras)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION);
 
 
 
 -- -----------------------------------------------------
--- Table Farmacia.Med_Fam
+-- Table Med_Fam
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS Farmacia.Med_Fam (
+CREATE TABLE IF NOT EXISTS Med_Fam (
   Cod_Barras VARCHAR(45) NOT NULL,
   Nomb_Familia VARCHAR(45) NOT NULL,
   PRIMARY KEY (Cod_Barras, Nomb_Familia),
   CONSTRAINT Cod_Barras
     FOREIGN KEY (Cod_Barras)
-    REFERENCES Farmacia.Medicamento (Cod_Barras)
+    REFERENCES Medicamento (Cod_Barras)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT Nomb_Familia
     FOREIGN KEY (Nomb_Familia)
-    REFERENCES Farmacia.Familia (Nomb_Familia)
+    REFERENCES Familia (Nomb_Familia)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION);
 
 
 
 -- -----------------------------------------------------
--- Table Farmacia.Med_Comp
+-- Table Med_Comp
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS Farmacia.Med_Comp (
+CREATE TABLE IF NOT EXISTS Med_Comp (
   Cod_Compra INT NOT NULL,
   Cod_Barras VARCHAR(45) NOT NULL,
   Cantidad INT NULL,
   PRIMARY KEY (Cod_Compra, Cod_Barras),
   CONSTRAINT Cod_Compra
     FOREIGN KEY (Cod_Compra)
-    REFERENCES Farmacia.Compra (Cod_Compra)
+    REFERENCES Compra (Cod_Compra)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT Cod_Barras
     FOREIGN KEY (Cod_Barras)
-    REFERENCES Farmacia.Medicamento (Cod_Barras)
+    REFERENCES Medicamento (Cod_Barras)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION);
 
 
 
 -- -----------------------------------------------------
--- Table Farmacia.Con_Credito_Comp
+-- Table Con_Credito_Comp
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS Farmacia.Con_Credito_Comp (
+CREATE TABLE IF NOT EXISTS Con_Credito_Comp (
   DNI VARCHAR(9) NOT NULL,
   Cod_Compra INT NULL,
   PRIMARY KEY (DNI),
   CONSTRAINT DNI
     FOREIGN KEY (DNI)
-    REFERENCES Farmacia.Con_Credito (DNI)
+    REFERENCES Con_Credito (DNI)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT Cod_Compra
     FOREIGN KEY (Cod_Compra)
-    REFERENCES Farmacia.Compra (Cod_Compra)
+    REFERENCES Compra (Cod_Compra)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION);
 
 
 
 -- -----------------------------------------------------
--- Table Farmacia.Normal_Comp
+-- Table Normal_Comp
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS Farmacia.Normal_Comp (
+CREATE TABLE IF NOT EXISTS Normal_Comp (
   DNI VARCHAR(9) NOT NULL,
   Cod_Compra INT NULL,
   PRIMARY KEY (DNI),
   CONSTRAINT DNI
     FOREIGN KEY (DNI)
-    REFERENCES Farmacia.C_Normal (DNI)
+    REFERENCES C_Normal (DNI)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT Cod_Compra
     FOREIGN KEY (Cod_Compra)
-    REFERENCES Farmacia.Compra (Cod_Compra)
+    REFERENCES Compra (Cod_Compra)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION);
